4b50d4ae278b8558fc0591cc93d53ba292447278
Gustavo Dechotti <75210025+GusDechotti@users.noreply.github.com>
2023-10-03 22:07:44 -0300

    commit

---
+++ schemas/index.js
@@ -0,0 +1,14 @@
+import certificados from "./certificados";
+import horarios from "./horarios";
+import noticias from "./noticias";
+import duvidas from "./duvidas";
+import equipe from "./equipe";
+import laboratorios from "./laboratorios";
+import lista from "./lista";
+import descricao_metodos from "./metodo";
+import clube from "./clube";
+import equipe2 from "./equipe2";
+import premios from "./premios";
+import preclube from "./preclube";
+
+export const schemaTypes = [horarios,certificados,noticias,duvidas,equipe, lista, descricao_metodos, laboratorios, clube, equipe2, preclube]
