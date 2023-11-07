cccf1b20cc26cd6fc698fc7e6f9e82eb73746a9c
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-11-07 15:31:08 -0300

    responsividade imagens laboratorios

--- js-dinamicos/laboratorios.js
+++ js-dinamicos/laboratorios.js
@@ -1,4 +1,4 @@
-const laboratoriosAPI = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22laboratorios%22%5D%0A%0A%0A%0A%0A%0A%0A";
+const laboratoriosAPI = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22laboratorios%22%5D%7B%0A++%22laboratorio%22%3A+laboratorio%2C%0A++%22descricao%22%3A+descricao%2C%0A++%22foto%22%3A+foto.asset-%3Eurl%2C%0A++%22fotopequena1%22%3A+fotoPequena1.asset-%3Eurl%2C%0A++%22fotopequena2%22%3A+fotoPequena2.asset-%3Eurl%2C%0A++%0A%7D";
 
 async function ListarTarefasLaboratorios() {
   const result = await fetch(laboratoriosAPI, {
@@ -7,7 +7,9 @@ async function ListarTarefasLaboratorios() {
 
   const data = await result.json();
   console.log(data);
+  console.log(data.result);
   console.log(data.result.result);
+
 
   const cardLaboratorio = document.getElementById("cards-laboratorios");
   console.log(cardLaboratorio);
@@ -34,45 +36,37 @@ async function ListarTarefasLaboratorios() {
     pDescricao.classList.add("desc-lab");
     pDescricao.innerText = laboratorio.descricao;
 
-    const picture = document.createElement("picture");
-
-    const source1 = document.createElement("source");
-    source1.setAttribute("media", "(max-width: 388px)");
-    source1.setAttribute("srcset", laboratorio.foto);
-
-    const source2 = document.createElement("source");
-    source2.setAttribute("media", "(max-width: 630px)");
-    source2.setAttribute("srcset", laboratorio.foto);
+    const divImagens = document.createElement("div");
 
     const img = document.createElement("img");
     img.setAttribute("src", laboratorio.foto);
-    img.setAttribute("alt", "");
 
-    picture.appendChild(source1);
-    picture.appendChild(source2);
-    picture.appendChild(img);
+    divImagens.appendChild(img);
 
     const cardImgNone = document.createElement("div");
     cardImgNone.classList.add("card-img-none");
 
     const imgPequena1 = document.createElement("img");
-    imgPequena1.setAttribute("src", laboratorio.fotoPequena1);
+    imgPequena1.setAttribute("src", laboratorio.fotopequena1);
     imgPequena1.setAttribute("alt", "");
 
     const imgPequena2 = document.createElement("img");
-    imgPequena2.setAttribute("src", laboratorio.fotoPequena2);
+    imgPequena2.setAttribute("src", laboratorio.fotopequena2);
     imgPequena2.setAttribute("alt", "");
 
     cardImgNone.appendChild(imgPequena1);
     cardImgNone.appendChild(imgPequena2);
+    divImagens.appendChild(cardImgNone);
+    
 
     if (contador % 2 !== 0) {
       card.classList.add("card");
       txt.appendChild(pTitulo);
       txt.appendChild(pDescricao);
       card.appendChild(txt);
-      card.appendChild(picture);
+      card.appendChild(divImagens);
       card.appendChild(cardImgNone);
+      divImagens.className = "card-img";
 
     } else {
       
@@ -80,8 +74,9 @@ async function ListarTarefasLaboratorios() {
       txt.appendChild(pTitulo);
       txt.appendChild(pDescricao);
       card.appendChild(txt);
-      card.appendChild(picture);
+      card.appendChild(divImagens);
       card.appendChild(cardImgNone);
+      divImagens.className = "card-img-reverse";
     }
 
     cardLaboratorio.appendChild(card);
