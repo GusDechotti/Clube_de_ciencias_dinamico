877a1b92767c4435ccdb2345d7904ddf53f9abb2
Mario Guilherme Wilhelms <marioguilhermew@gmail.com>
2023-11-14 09:07:00 -0300

    equipe finalizada

--- js-dinamicos/equipe.js
+++ js-dinamicos/equipe.js
@@ -1,8 +1,8 @@
 
 
-// Endpoint pego no query do Sanity
+// Endpoint pego no query do Sanity limitado a 8!
 
-const endpointEquipe = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22duvidas%22%5D%0A%0A%0A"
+const endpointEquipe = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27equipe%27%5D+%7C+order%28_createdAt+asc%29+%5B0...8%5D+%7B%0A++%27Nome%27%3A+name%2C%0A++%27Cargo%27%3A+cargo%2C%0A++%27Imagem%27%3A+foto.asset-%3Eurl%2C%0A%7D"
 
 async function ListarEquipe() {
 
@@ -10,11 +10,11 @@ async function ListarEquipe() {
       method: "GET",
     });
 
-    const apiDuvidasJson = resultadoApiDuvidas.json()
+    const apiDuvidasJson = await resultadoApiDuvidas.json()
        
-    const apiDuvidas = apiDuvidasJson.result[0]
+    const apiDuvidas = apiDuvidasJson.result
 
-    const equipeDiv = document.getElementById("sep")
+    const equipeDiv = document.querySelector("#sep")
 
     // Zera a div de equipe para cadastrar todos membros sempre que adicionado
 
@@ -22,37 +22,37 @@ async function ListarEquipe() {
 
     // Adiciona todos os membros da lista de integrantes da equipe
 
-      apiDuvidas.forEach(member => {
+    apiDuvidas.forEach(member => {
 
-        // Cria a div do integrante com sua classe
+      // Cria a div do integrante com sua classe
 
-        const profDiv = document.createElement("div")
-        profDiv.className = "profs"
+      const profDiv = document.createElement("div")
+      profDiv.className = "profs"
 
-        // Cria a imagem do integrante
+      // Cria a imagem do integrante
 
-        const imgElement = document.createElement("img")
-        imgElement.src = member.Imagem
+      const imgElement = document.createElement("img")
+      imgElement.src = member.Imagem
 
-        // Cria as 
+      // Cria div das informações(nome/cargo)
 
-        const nomeDiv = document.createElement("div")
-        nomeDiv.id = "nome"
+      const nomeDiv = document.createElement("div")
+      nomeDiv.id = "nome"
 
-        const h3Element = document.createElement("h3")
-        h3Element.textContent = member.Nome
+      const h3Element = document.createElement("h3")
+      h3Element.textContent = member.Nome
 
-        const pElement = document.createElement("p")
-        pElement.textContent = member.Cargo;
+      const pElement = document.createElement("p")
+      pElement.textContent = member.Cargo;
 
-        nomeDiv.appendChild(h3Element)
-        nomeDiv.appendChild(pElement)
+      nomeDiv.appendChild(h3Element)
+      nomeDiv.appendChild(pElement)
 
-        profDiv.appendChild(imgElement)
-        profDiv.appendChild(nomeDiv)
+      profDiv.appendChild(imgElement)
+      profDiv.appendChild(nomeDiv)
 
-        equipeDiv.appendChild(profDiv)
-      })
+      equipeDiv.appendChild(profDiv)
+    })
   }
 
-atualizarEquipe();
+ListarEquipe();
