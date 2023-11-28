fd4d80d0760006b2729480f906318e4a283c785c
Mario Guilherme Wilhelms <126964100+marioguiW@users.noreply.github.com>
2023-11-28 10:00:57 -0300

    agora sim

--- js-dinamicos/duvidas.js
+++ js-dinamicos/duvidas.js
@@ -2,7 +2,7 @@
 
 // Endpoint pego no query do Sanity
 
-const endpointDuvidas = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%22duvidas%22%5D%0A%0A%0A"
+const endpointDuvidas = "https://hmwoh9gp.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type+%3D%3D+%27duvidas%27%5D+%7C+order%28_createdAt+desc%29+%5B0...6%5D+%7B%0A++%22Pergunta%22%3A+pergunta%2C%0A++%22Resposta%22%3A+respostas%0A%7D"
 
 async function duvidas(){
   const resultadoApiDuvidas = await fetch(endpointDuvidas, {
@@ -17,15 +17,13 @@ async function duvidas(){
     
     const cardDuvida = document.getElementById("perguntas-frequentes")
 
+    console.log(apiDuvidas)
+
     // Criando variavel contadora para inserir um id auto_increment
 
     var contadora = 0
 
-    console.log(apiDuvidas)
-
     apiDuvidas.forEach(duvidaInfo =>{
-
-      console.log(duvidaInfo)
 
       // Cria um paragrafo e o botão, e passa os atributos para funcionar o modal do botão
 
@@ -40,7 +38,7 @@ async function duvidas(){
 
       // Insere a pergunta da lista de perguntas ao botão
 
-      botaoPergunta.innerText = duvidaInfo.pergunta
+      botaoPergunta.innerText = duvidaInfo.Pergunta
 
       // Cria div resposta 
 
@@ -53,7 +51,7 @@ async function duvidas(){
 
       // insere a resposta na div resposta
 
-      divCardResposta.innerText = duvidaInfo.respostas
+      divCardResposta.innerText = duvidaInfo.Resposta
 
       paragrafo.appendChild(botaoPergunta)
 
